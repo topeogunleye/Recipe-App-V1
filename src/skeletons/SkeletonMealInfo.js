caced711709a4bb9b748189e213cc8c9e60d290a
Temitope Ogunleye <topeogunleye1@gmail.com>
2021-04-25 17:38:59 +0100

    Adds _redirects file to redirect all the routes to the index.html

--- src/skeletons/SkeletonMealInfo.js
+++ src/skeletons/SkeletonMealInfo.js
@@ -7,30 +7,59 @@ const SkeletonMealInfo = ({ theme }) => {
   return (
     <div className={`skeleton-wrapper ${themeClass}`}>
       <div className="skeleton-article flex flex-col items-center justify-center text-center">
-        <SkeletonElement type="title"/>
-        <SkeletonElement type="imageBig"/>
-        <SkeletonElement type ="mealInfo"/>
-        <SkeletonElement type ="mealInfo"/>
-        <SkeletonElement type = "textBig"/>
+        <SkeletonElement type="title" />
+        <SkeletonElement type="imageBig" />
+        <SkeletonElement type="mealInfo" />
+        <SkeletonElement type="mealInfo" />
+        <SkeletonElement type="textBig" />
         <ul className="pl-0">
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li><li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li><li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
-        <li className="border-2 border-solid border-gray-200 rounded bg-white inline-block text-gray-900 text-xs font-bold p-1 mt-0 mr-1 mb-1 ml-0"><SkeletonElement type = "liText"/></li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mb-0 mr-1 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mb-0 mr-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
+          <li className="rounded inline-block text-gray-900 text-xs font-bold p-0 mt-0 mr-1 mb-0 ml-0">
+            <SkeletonElement type="liText" />
+          </li>
         </ul>
       </div>
       <Shimmer />
     </div>
-  )
-}
-
+  );
+};
 
 export default SkeletonMealInfo;
